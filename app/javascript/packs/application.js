0ffff436982c7451b2080f5812c6afa0a8814eab
vijaytembugade <vijay.tembugade@zendesk.com>
2025-09-28 01:21:28 +0530

    Added React.js to the project

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,16 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "../stylesheets/application.scss"
+import ReactRailsUJS from "react_ujs";
+import App from "../src/App";
 
 import { setAuthHeaders } from "apis/axios";
 import { initializeLogger } from "common/logger";
 
 setAuthHeaders();
 initializeLogger();
+
+const componentsContext = { App };
+ReactRailsUJS.getConstructor = (name) => {
+  return componentsContext[name];
+};
